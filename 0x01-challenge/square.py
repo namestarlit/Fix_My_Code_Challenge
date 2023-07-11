c523efb7915bf28da9de0a6114f01fa8d0e010a2
Paul John <rainson.work@gmail.com>
2023-07-11 19:26:37 +0300

    Fix `square.py`

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
 
-class Square:
-    """Representantion of a square class"""
-    side = 0
+class Square():
+    """Representantion of a Square class"""
+    width = 0
+    height = 0
 
     def __init__(self, *args, **kwargs):
-        """Initialization of square object"""
+        """Initialization of class object"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.side * self.side
+        return self.width * self.height
 
-    def perimeter_of_my_square(self):
-        """ Perimeter of the square """
-        return 4 * self.side
+    def PermiterOfMySquare(self):
+        """Perimeter of a Square"""
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """String Representantion of Square"""
-        return "{}/{}".format(self.side, self.side)
+        """String representantion of Square"""
+        return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    s = Square(side=12)
+
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.perimeter_of_my_square())
+    print(s.PermiterOfMySquare())
